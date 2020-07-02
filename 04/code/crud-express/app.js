7fdf648a98714ecf8d9a0f38c1b0f52525b188a1
韦荣艺 <1099393790@qq.com>
2020-07-02 14:43:17 +0800

    下载模板引擎并渲染首页

--- 04/code/crud-express/app.js
+++ 04/code/crud-express/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+// 配置模板引擎、
+app.engine('html', require('express-art-template'));
+// 首页路由
 app.get('/', function (req, res) {
-	res.send('hello world');
+	res.render('index.html');
 })
 app.listen(3000, function () {
 	console.log('express is runing...');
