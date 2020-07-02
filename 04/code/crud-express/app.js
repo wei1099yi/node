bc793f189a6bd38fb37f23f9583582144e1634d9
韦荣艺 <1099393790@qq.com>
2020-07-02 16:45:50 +0800

    从文件读取数据并实现基础渲染

--- 04/code/crud-express/app.js
+++ 04/code/crud-express/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 // 开放静态资源文件夹
 app.use('/node-modules', express.static('./node_modules/'));
@@ -8,14 +9,22 @@ app.use('/public/', express.static('./public/'));
 app.engine('html', require('express-art-template'));
 // 首页路由
 app.get('/', function (req, res) {
-	res.render('index.html', {
-		fruits: [
-			'苹果',
-			'香蕉',
-			'橘子',
-			'葡萄'
-		]
-	});
+	// 第二个参数 ’utf-8‘ 表示将取到的数据以固定格式转换成我们能识别的字符串
+	fs.readFile('./db.json', 'utf-8', (err, data) => {
+		if (err) return res.status(500).send('Server error!');
+		console.log(typeof(data));  // string
+		// 取到文件的数据后通过模板引擎渲染并发送给浏览器
+		res.render('index.html', {
+			fruits: [
+				'苹果',
+				'香蕉',
+				'橘子',
+				'葡萄'
+			],
+			students: JSON.parse(data).students
+		});
+
+	})
 })
 app.listen(3000, function () {
 	console.log('express is runing...');
