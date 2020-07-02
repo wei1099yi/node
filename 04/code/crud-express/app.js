bfd7a1bf9402914361412ef36353fc1db9a6f1cf
韦荣艺 <1099393790@qq.com>
2020-07-02 15:54:47 +0800

    下载 bootstrap 并完成首页布局

--- 04/code/crud-express/app.js
+++ 04/code/crud-express/app.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
 
+// 开放静态资源文件夹
+app.use('/node-modules', express.static('./node_modules/'));
+app.use('/public/', express.static('./public/'));
 // 配置模板引擎、
 app.engine('html', require('express-art-template'));
 // 首页路由
 app.get('/', function (req, res) {
-	res.render('index.html');
+	res.render('index.html', {
+		fruits: [
+			'苹果',
+			'香蕉',
+			'橘子',
+			'葡萄'
+		]
+	});
 })
 app.listen(3000, function () {
 	console.log('express is runing...');
