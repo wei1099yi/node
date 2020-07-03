95d712b46c795253410305c24459dd55ecb29d22
韦荣艺 <1099393790@qq.com>
2020-07-03 18:02:47 +0800

    使用中间件获取 post 请求数据

--- 04/code/crud-express/app.js
+++ 04/code/crud-express/app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 // 引用 路由模块
 var router = require('./router');
+
 var app = express();
+
 // 开放静态资源文件夹
 app.use('/node-modules', express.static('./node_modules/'));
 app.use('/public/', express.static('./public/'));
+// 使用主体解析中间件获取 post 请求数据
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 把路由容器挂载到 app 服务中
+app.use(router);
+
 // 配置模板引擎、
 app.engine('html', require('express-art-template'));
 
-// 把路由容器挂载到 app 服务中
-app.use(router);
 app.listen(3000, function () {
 	console.log('express is runing...');
 })
